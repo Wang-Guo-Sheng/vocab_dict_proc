adccf7b6ae4ca34940498ce1dd73cc5aad350a6d
Wang Guosheng <wguosh@gmail.com>
2021-11-24 14:42:52 +0800

    edit

--- sort_freq.sh
+++ sort_freq.sh
@@ -1,11 +1,22 @@
 #/usr/bin/zsh
 
 if [ ! -f tmp_freq ]; then
-    while read w; do
-        grep -E "\b$w\b" unigram_freq.csv -n | tr ':' ' ' >> tmp_freq
-    done < $1
-    sort -k1 -n tmp_freq
-    rm tmp_freq
+	notfound=()
+	while read w; do
+        if [ ! -z "${w}" ]; then
+            newline=$(grep -E "\b$w\b" unigram_freq.csv -n)
+        	if [ -z "${newline}" ]; then
+        			notfound+=(${w})
+        		else
+        		echo ${newline} | tr ':' ' ' >> tmp_freq
+        	fi
+        fi
+	done < $1
+	sort -k1 -n tmp_freq
+	for w in ${notfound[@]}; do
+	echo $w
+	done
+	rm tmp_freq
 else
-    echo "Rename tmp_freq."
+	echo "Rename tmp_freq."
 fi
